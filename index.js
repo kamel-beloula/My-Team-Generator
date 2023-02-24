478077105823188f8fa561ddf2298687b95e83be
kamel beloula <kamel.solo@live.com>
2023-02-24 19:55:12 +0000

    added a manager's prompt

--- index.js
+++ index.js
@@ -9,15 +9,48 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
+const EmployeeArray = [];
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //structure of the code provided by the class instructor
 
 inquirer.prompt([{
+    console.log(`Start building your software team`);
     //manager questions
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter the Manager's name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter the Manager's ID:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Pease enter the Manager's email address:",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter the Manager's office number",
+        },
+    ])
+
 }]).then(response => {
+    console.log(`Manager's entry complete. Please provide the informations for the rest of the team members`)
     // populate manager info
+    const manager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber,
+    )
     // promptForNexEmployee ()
 })
 
