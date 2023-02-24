b0a88a1e2eaa6b0ac1b3294cf5408db535dc38c8
kamel beloula <kamel.solo@live.com>
2023-02-24 21:56:30 +0000

    added new engineer infos and pushed it to the employees array

--- index.js
+++ index.js
@@ -12,118 +12,134 @@ const render = require("./src/page-template.js");
 const Employee = require("./lib/Employee");
 const employees = [];
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //structure of the code provided by the class instructor
 
-    console.log(`Start building your software engineering team`),
-    //manager questions
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter the Manager's name:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter the Manager's ID:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Pease enter the Manager's email address:",
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Please enter the Manager's office number",
-        },
+console.log(`Start building your software engineering team`),
+  //manager questions
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the Manager's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter the Manager's ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Pease enter the Manager's email address:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the Manager's office number",
+      },
     ])
 
-    .then(response => {
-    console.log(`Manager's entry complete. Please provide the informations for the rest of the team members`)
-    // populate manager info
-    const manager = new Manager(
+    .then((response) => {
+      console.log(
+        `Manager's entry complete. Please provide the informations for the rest of the team members`
+      );
+      // populate manager info
+      const manager = new Manager(
         response.name,
         response.id,
         response.email,
-        response.officeNumber,
-    );
-    employees.push(manager);
-    console.log(employees);
+        response.officeNumber
+      );
+      employees.push(manager);
 
-    // promptForNexEmployee ()
-    promptForNextEmployee()
-})
+      // promptForNexEmployee ()
+      promptForNextEmployee();
+    });
 
 const promptForNextEmployee = () => {
-    inquirer.prompt([{
-        type: 'list',
-        message: 'Please choose the next employee role',
-        name: 'addMemberOrFinish',
-        choices: [
-            "Engineer",
-            "Intern",
-            "Finish building the team",
-        ]}
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose the next employee role",
+        name: "addMemberOrFinish",
+        choices: ["Engineer", "Intern", "Finish building the team"],
+      },
     ])
-        .then(response => {
-        // if engineer
-        if(response.addMemberOrFinish === "Engineer"){
+    .then((response) => {
+      // if engineer
+      if (response.addMemberOrFinish === "Engineer") {
         // promptForEngineer
-            promptForEngineer();
+        promptForEngineer();
         // else if intern
-        }else if(response.addMemberOrFinish === "Intern"){
+      } else if (response.addMemberOrFinish === "Intern") {
         // promptForIntern
-            promptForIntern();
+        promptForIntern();
         // else
-        }else{
+      } else {
         //use the functionality from page-template to generate the team
-            buildPage();
-        }
-    })
-}
+        buildPage();
+      }
+    });
+};
 
 const promptForEngineer = () => {
-    inquirer.prompt([
-        //engineer questions
-        {
-            type: "input",
-            name: "name",
-            message: "Please enter their name:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please enter their ID:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Pease enter their email address:",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Please their github user name:",
-        },
+  inquirer
+    .prompt([
+      //engineer questions
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter their name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Please enter their ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Pease enter their email address:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Please their github user name:",
+      },
+    ])
+    .then((response) => {
+      // populate engineer info
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      // add new engineer to employees array
+      employees.push(emgineer);
+      console.log(employees);
 
-    ]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
-    })
-}
+      // promptForNexEmployee ()
+      promptForNextEmployee();
+    });
+};
 
 const promptForIntern = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         //intern questions
-    }]).then(response => {
-        // add new intern to employees array
-        // promptForNextEmployee
-    })
-}
+      },
+    ])
+    .then((response) => {
+      // add new intern to employees array
+      // promptForNextEmployee
+    });
+};
 
 const buildPage = () => {
-// render(myArrayOfTeamMembers)
-}
+  // render(myArrayOfTeamMembers)
+};
